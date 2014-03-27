783337fd0684a9d7cda1f3bb402a5876449535cb
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2014-03-26 22:55:49 -0400

    adding users and accounts with warden, adding warden middelware to lib/subscribem/engine.rb

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -4,11 +4,13 @@ feature "Accounts" do
     visit subscribem.root_path
     click_link "Account Sign Up"
     fill_in "Name", :with => "Test"
-    fill_in "Email", :with => "subcribem@example.com"
+    fill_in "Email", :with => "subscribem@example.com"
     fill_in "Password", :with => 'password', :exact => true
     fill_in "Password confirmation", :with => "password"
     click_button "Create Account"
     success_message = "Your account has been successfully created."
+    email_message = "Signed in as subscribem@example.com"
     page.should have_content(success_message)
+    page.should have_content(email_message)
   end
 end
