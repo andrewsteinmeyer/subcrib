9b9102a2f119fdc02a879dd7cb584342318eeae0
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2014-03-27 23:43:38 -0400

    added subdomain routing and authentication

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -13,6 +13,49 @@ feature "Accounts" do
     email_message = "Signed in as subscribem@example.com"
     page.should have_content(success_message)
     page.should have_content(email_message)
-    page.current_url.should == "http://test.example.com/subscribem"
+    page.current_url.should == "http://test.example.com/"
+  end
+
+  scenario "Ensure subdomain uniqueness" do
+    Subscribem::Account.create!(:subdomain => "test", :name => "Test") 
+    visit subscribem.root_path
+    click_link "Account Sign Up"
+    fill_in "Name", :with => "Test"
+    fill_in "Subdomain", :with => "test"
+    fill_in "Email", :with => "subscribem@example.com"
+    fill_in "Password", :with => 'password', :exact => true
+    fill_in "Password confirmation", :with => "password"
+    click_button "Create Account"
+    page.current_url.should == "http://www.example.com/accounts"
+    page.should have_content("Sorry, your account could not be created.")
+    page.should have_content("Subdomain has already been taken")
+  end
+
+  scenario "Subdomain with restricted name" do
+    visit subscribem.root_path
+    click_link "Account Sign Up"
+    fill_in "Name", :with => "Test"
+    fill_in "Subdomain", :with => "admin"
+    fill_in "Email", :with => "subscribem@example.com"
+    fill_in "Password", :with => 'password', :exact => true
+    fill_in "Password confirmation", :with => "password"
+    click_button "Create Account"
+    page.current_url.should == "http://www.example.com/accounts"
+    page.should have_content("Sorry, your account could not be created.")
+    page.should have_content("Subdomain is not allowed.  Please choose another subdomain.") 
+  end
+
+  scenario "Subdomain with invalid name" do
+    visit subscribem.root_path
+    click_link "Account Sign Up"
+    fill_in "Name", :with => "Test"
+    fill_in "Subdomain", :with => "<admin>"
+    fill_in "Email", :with => "subscribem@example.com"
+    fill_in "Password", :with => 'password', :exact => true
+    fill_in "Password confirmation", :with => "password"
+    click_button "Create Account"
+    page.current_url.should == "http://www.example.com/accounts"
+    page.should have_content("Sorry, your account could not be created.")
+    page.should have_content("Subdomain is not allowed.  Please choose another subdomain.") 
   end
 end
--- subscribem.gemspec
+++ subscribem.gemspec
@@ -23,4 +23,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency "capybara", '2.1.0'
   s.add_development_dependency "bcrypt-ruby", '3.1.2'
   s.add_development_dependency "warden", '1.2.3'
+  s.add_dependency "dynamic_form", "1.1.4"
+  s.add_development_dependency "factory_girl", "4.1.0"
 end
